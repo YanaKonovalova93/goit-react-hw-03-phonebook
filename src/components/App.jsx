8fdf7a0d8e758940b6d78028ba202dcdcd07832e
YanaKonovalova93 <mikobacteria@gmail.com>
2023-07-01 12:15:04 +0300

    finish project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,7 @@ import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -62,6 +57,21 @@ export class App extends Component {
       name.toLowerCase().includes(this.state.filter.toLowerCase())
     );
   };
+
+  componentDidMount() {
+    const stringifiedContacts = localStorage.getItem('contacts');
+    const contacts = JSON.parse(stringifiedContacts) ?? [];
+
+    this.setState({ contacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      const stringifiedContacts = JSON.stringify(this.state.contacts);
+      localStorage.setItem('contacts', stringifiedContacts);
+    }
+  }
 
   render() {
     const filteredContacts = this.getFilterContacts();
